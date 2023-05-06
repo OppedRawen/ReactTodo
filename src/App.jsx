a1097645b281efef141a372ebb10ffced5598431
OppedRawen <yud2372@gmail.com>
2023-05-06 09:44:38 -0700

    third

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,43 @@ import { useState } from "react"
 
 // states to make stuff responsive
 // useState is a hook from react
+// states cannnot be changed
+// we have to return a new state everytime
 export default function App(){
- const[newItem,setNewItem]= useState("sdsdsd");
+ const[newItem,setNewItem]= useState("");
+ const [todos, setTodos] = useState([]);
 
-  return <><form className="new-item-form">
+  function handleSubmit(e){
+    e.preventDefault();
+    // to make sure that this can repeat, make setTodos into a function
+    // if you want to use the current value, always pass it in a function
+    setTodos(currentTodos=>{
+      return [...currentTodos, {id: crypto.randomUUID(), title:newItem, completed:false}];
+     });
+
+     setNewItem("");
+    // setTodos([...todos, {id: crypto.randomUUID(), title:newItem, completed:false}]);
+  }
+  function toggleTodo(id,completed){
+    setTodos(currentTodos=>{
+      return currentTodos.map(todo=>{
+        if(todo.id ===id){
+          return {...todo,completed}
+        }
+        return todo;
+      })
+    })
+  }
+function deleteTodo(id){
+  setTodos(currentTodos=>{
+    return currentTodos.filter(todo=>todo.id!==id);
+  })
+}
+
+  // to loop through stuff in react html, we need to use a map
+  // we have to give a key to every element in react that we loop through to know whcih one we change
+ console.log(todos);
+  return <><form onSubmit={handleSubmit} className="new-item-form">
    
     <div className="form -row">
       <label htmlFor="item">New Item</label>
@@ -19,23 +52,19 @@ export default function App(){
     <button className="btn">Add</button>
   </form>
   <h1 className="header">Todo List</h1>
+  
   <ul className="list">
-    <li>
+    {todos.map(todo=>{
+      return  <li key={todo.id}>
       <label>
-        <input type="checkbox">
+        <input type="checkbox" checked={todo.completed} onChange={e=>toggleTodo(todo.id,e.target.checked)}>
         </input>
-        Item 1
+        {todo.title}
       </label>
-      <button className="btn btn-danger">Delete</button>
+      <button onClick={()=>deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
     </li>
-    <li>
-      <label>
-        <input type="checkbox">
-        </input>
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    })}
+
   </ul>
   </>
 }
